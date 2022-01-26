d3cdacebaa41d469848c7c65302cc0b8a7fcb4b5
codesinging <codesinging@gmail.com>
2022-01-26 10:12:26 +0800

    增加基础异常类

---
+++ src/Exceptions/Exception.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Author: codesinging <codesinging@gmail.com>
+ * Github: https://github.com/codesinging
+ */
+
+namespace CodeSinging\PinAdmin\Exceptions;
+
+class Exception extends \Exception
+{
+
+}
