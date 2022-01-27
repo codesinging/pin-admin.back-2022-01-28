71b58189915647b9ec97dde8034d2998b223bdf4
codesinging <codesinging@gmail.com>
2022-01-27 16:40:22 +0800

    完善路由文件模板

--- stubs/routes.php
+++ stubs/routes.php
@@ -3,3 +3,23 @@
  * Author: codesinging <codesinging@gmail.com>
  * Github: https://github.com/codesinging
  */
+
+use Illuminate\Support\Facades\Route;
+
+// 无需登录授权的路由
+Route::middleware(['web', 'admin.guest:__DUMMY_NAME__'])
+    ->prefix('__DUMMY_PREFIX__')
+    ->group(function () {
+
+        Route::get('/auth/login', [__DUMMY_NAMESPACE__\Controllers\AuthController::class, 'index']);
+
+    });
+
+// 需要登录授权的路由
+Route::middleware(['web', 'admin.auth:__DUMMY_NAME__,__DUMMY_LABEL_____DUMMY_NAME__'])
+    ->prefix('__DUMMY_PREFIX__')
+    ->group(function () {
+
+        Route::get('/', [__DUMMY_NAMESPACE__\Controllers\IndexController::class, 'index']);
+
+    });
